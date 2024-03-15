cf653c231875cde105c4f08839243feea5c5910a
Jiraded <jiraded27@gmail.com>
2024-03-15 13:42:25 +0700

    add auth

---
+++ src/config/firebase.ts
@@ -0,0 +1,19 @@
+"use client"
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqy9miTgxFUazy9MYPBl74lZmFpvduueo",
+  authDomain: "jenosize-test-9d706.firebaseapp.com",
+  projectId: "jenosize-test-9d706",
+  storageBucket: "jenosize-test-9d706.appspot.com",
+  messagingSenderId: "24127279582",
+  appId: "1:24127279582:web:a5081462c95f6db8b80d4f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
