5cbabd7b121335f2c4a06a45bd49ce845d623628
Jiraded <jiraded27@gmail.com>
2024-03-17 22:06:26 +0700

    fix fontawesome ssr css problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import "@/app/globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Inter } from "next/font/google";
+
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
 
